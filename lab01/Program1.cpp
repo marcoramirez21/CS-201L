bd1c5edc0a8e9f2a0ff5cb9e37870a2175328453
Marco Ramirez <marccoramirez.21gmail.com>
2025-06-11 20:59:04 -0500

    Fix grammar and spelling in comments

--- lab01/Program1.cpp
+++ lab01/Program1.cpp
@@ -2,9 +2,9 @@
 //Program 1 - Numbers
 //Analyze 4 input numbers to output the largest, smallest, average, and sort them. 
 
-#include <iostream>// library for stantar input/output
-#include<algorithm>// gives us std::swap, a simple way to exchange values
-#include<cctype>// gives us toupper(), whick helps hande both 'y' and 'Y'
+#include <iostream>// library for standar input/output
+#include<algorithm>// Provides std::swap, a simple way to exchange values
+#include<cctype>// Provides toupper(), which helps handle both 'y' and 'Y'
 using namespace std;// So we don't need to prefix std:: everywhere
 
 int main() {
@@ -16,11 +16,11 @@ int main() {
 		cout << "Enter four integers: "; //prompt user
 		cin >> a >> b >> c >> d; // input four integers from user
 
-		// Copy orginal values to sort whithout affecting the original input
+		// Copy original values to sort without affecting the original input
 		int n1 = a, n2 = b, n3 = c, n4 = d;
 		
-		/* Sorting using bubble sort logi with swap
-		Bubble sort repeateddly compares adjacent values and swaps them 
+		/* Sorting using bubble sort logic with swap
+		bubble sort repeatedly compares adjacent values and swaps them 
 		to bubble larger values toward the end (ascending order) */
 
 		
@@ -34,20 +34,20 @@ int main() {
 		if (n1 > n2) swap(n1, n2); // final check to make sure smallest is first 
 		// Now n1 to n4 are sorted in increasing order
 
-		//Since is alredy sorted, largest is last(n4), smallest is firts(n1)
+		//Since it's already sorted, largest is last(n4), smallest is first(n1)
 		cout <<"The largest number is: " << n4 << endl; // print largest number
 		cout << "The smallest number is: " << n1 << endl;// print smallest number
 
 		// Calculate average using original values.
-		double avg = (a + b + c + d) / 4.0 ; // double: to ensure floating-point division
+		double avg = (a + b + c + d) / 4.0; // Use "double" to ensure floating-point division
 		cout << "The averge is: " << avg << endl;
 
-		//Display sorthed numbers 
+		//Display sorted numbers 
 		cout << "Numbers in increasing order: "; 
-		cout << n1 << "," << n2 << "," << n3 << "," << n4 << endl; // print incresing order
+		cout << n1 << "," << n2 << "," << n3 << "," << n4 << endl; // print increasing order
 
 		cout << "Numbers in decreasing order: ";
-		cout << n4 << "," << n3 << "," << n2 << "," << n1 << endl; // print decresing order
+		cout << n4 << "," << n3 << "," << n2 << "," << n1 << endl; // print decreasing order
 	
 		//Ask user if they want to reapeat
 		cout << "Again (Y/N)?";
